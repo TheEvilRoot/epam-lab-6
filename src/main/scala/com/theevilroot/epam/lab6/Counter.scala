feb3c33c11cdf95f8e4c95c48a52bebfed65b5b9
TheEvilRoot <robot-creatix@ya.ru>
2020-04-29 14:35:41 +0300

    High-level functions

--- src/main/scala/com/theevilroot/epam/lab6/Counter.scala
+++ src/main/scala/com/theevilroot/epam/lab6/Counter.scala
@@ -5,9 +5,9 @@ import scala.annotation.tailrec
 object Counter {
 
   @tailrec
-  def count[A](in: List[A], value: A, c: Int = 0): Int = in match {
-    case a :: rest if a == value => count(rest, value, c + 1)
-    case _ :: rest => count(rest, value, c)
+  def count[A](in: List[A], value: A, compare: (A, A) => Boolean, c: Int = 0): Int = in match {
+    case a :: rest if compare(a, value) => count(rest, value, compare, c + 1)
+    case _ :: rest => count(rest, value, compare, c)
     case Nil => c
   }
 
--- src/test/scala/com/theevilroot/epam/lab6/unit/CounterTests.scala
+++ src/test/scala/com/theevilroot/epam/lab6/unit/CounterTests.scala
@@ -6,30 +6,31 @@ import org.scalatest.flatspec.AnyFlatSpec
 class CounterTests extends AnyFlatSpec {
 
   "Counter function" should "return 0 on empty collection" in {
-    assert(Counter.count(List(), 2) == 0)
+    assert(Counter.count(List(), 2, (x: Int, y: Int) => x == y) == 0)
   }
 
   it should "return 0 on collection without specified element" in {
-    assert(Counter.count(List(1, 2, 3), 4) == 0)
+    assert(Counter.count(List(1, 2, 3), 4, (x: Int, y: Int) => x == y) == 0)
   }
 
   it should "return size of collection when all collection's elements are equals to specified one" in {
     val collection = List(2, 2, 2, 2, 2)
-    assert(Counter.count(collection, 2) == collection.size)
+    assert(Counter.count(collection, 2, (x: Int, y: Int) => x == y) == collection.size)
   }
 
   it should "return 4 when collection contains 4 specified elements" in {
-    assert(Counter.count(List("A", "B", "B", "C", "B", "D", "B", "E"), "B") == 4)
+    assert(Counter.count(List("A", "B", "B", "C", "B", "D", "B", "E"), "B", (x: String, y: String) => x.equals(y)) == 4)
   }
 
-  it should "compare elements by it's equals function" in {
-    assert(Counter.count(List("A", "b", "B", "d", "B", "d", "b", "E"), "B") == 2)
+  it should "compare elements by compare function" in {
+    assert(Counter.count(List("A", "b", "B", "d", "B", "d", "b", "E"), "B", (x: String, y: String) =>
+      x.toLowerCase().equals(y.toLowerCase())) == 4)
   }
 
   it should "not modify collection" in {
     val origin = List(2, 3, 4, 5, 6, 5, 5, 4, 5, 2)
     val collection = origin
-    Counter.count(collection, 2)
+    Counter.count(collection, 2, (x: Int, y:Int) => x == y)
     assert(origin == collection)
   }
 
